39b1b962406a3fd5332e146c3d654d3d9b3ce08f
dazo-rollan <olandazo37@gmail.com>
2025-03-24 21:16:44 +0800

    add TODOs for main.py with task assignments

---
+++ main.py
@@ -0,0 +1,22 @@
+#TODO: Implement a function that adds an item to a products list
+# Assigned to: Agulto, Jermaine Razehl
+
+#TODO: Implement a function that checks if the customer is a senior citizen
+# Assigned to: Jundam, Kyle
+
+#TODO: Implement a function that totals the price of the items in the products 
+# list.
+# MUST Take into account the senior citizen discount
+# Assigned to: Dazo, Rollan
+
+#TODO: Implement a function that displays the the following details:
+# - Items(product name, price, quantity, total)
+# - Customer Name
+# - Senior ID no.
+# - Grand Total
+# Assigned to: Olazo, John Albert
+
+#TODO: Implement the main function that will call the functions above and
+# will hold the main logic of the program
+# Assigned to: Serohijos, Joshua
+#              Jundam, Kyle
